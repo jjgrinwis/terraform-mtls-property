09155c77e9805ee3ffec9416419f009436c24e31
John Grinwis <john@grinwis.com>
2025-06-16 17:31:19 +0200

    removed dv output

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ output "hostnames" {
   description = "Hostnames used in the property"
   value       = local.hostnames
 }
-output "dv_records" {
+/* output "dv_records" {
   description = "Our CNAME records for SBD will also contain the deployment status"
   value       = local.dv_records
-}
+} */
 output "property_name" {
   description = "Name the property and cpcode"
   value       = local.property_name
